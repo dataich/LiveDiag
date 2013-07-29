4c253a70c1f34ca1d3b27b22041891029f89b73e
Taichiro Yoshida <taichiro.yoshida@gmail.com>
2013-07-29 23:44:43 +0900

    #1 create Document-Based Cocoa application Xcode project

---
+++ LiveDiag/LDDocument.h
@@ -0,0 +1,13 @@
+//
+//  LDDocument.h
+//  LiveDiag
+//
+//  Created by Taichiro Yoshida on 2013/07/29.
+//  Copyright (c) 2013年 Taichiro Yoshida. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LDDocument : NSDocument
+
+@end
