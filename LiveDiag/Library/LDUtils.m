e6d4dd79658d55eb57ad0b37f1ebeedbcdb9c999
Shintaro Kaneko <kaneshin0120@gmail.com>
2015-02-12 22:06:10 +0900

    Detect command path when launching

--- LiveDiag/Library/LDUtils.m
+++ LiveDiag/Library/LDUtils.m
@@ -12,7 +12,32 @@
 
 + (NSString *)pathTo:(NSString *)command {
     NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-    return [userDefaults stringForKey:[NSString stringWithFormat:@"path_%@", command]];
+    NSString *path = [userDefaults stringForKey:[NSString stringWithFormat:@"path_%@", command]];
+    if (path == nil || [path isEqualTo:@""]) {
+        path = [LDUtils setPathWithCommand:command];
+    }
+    return path;
+}
+
++ (NSString *)setPathWithCommand:(NSString *)command {
+    NSTask *task = [[NSTask alloc] init];
+    NSPipe *pipe = [NSPipe pipe];
+    [task setStandardOutput:pipe];
+    [task setCurrentDirectoryPath:[[NSBundle mainBundle].bundlePath stringByDeletingLastPathComponent]];
+
+    [task setLaunchPath:@"/bin/bash"];
+    NSString *run = [NSString stringWithFormat:@"which %@", command];
+    NSArray *args = [NSArray arrayWithObjects:@"-l", @"-c", run, nil];
+    [task setArguments:args];
+    [task launch];
+    [task waitUntilExit];
+
+    NSData *data = pipe.fileHandleForReading.availableData;
+    NSString *path = [NSString stringWithFormat:@"%s", data.bytes];
+    path = [path stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setValue:path forKey:[NSString stringWithFormat:@"path_%@", command]];
+    return path;
 }
 
 @end
